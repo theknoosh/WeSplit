0f6051199a75699ab54835fea2109b14a1abc9b8
Darrell Payne <darrellpayne@earthlink.net>
2019-12-12 07:53:00 -0800

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by DARRELL A PAYNE on 12/12/19.
+//  Copyright © 2019 DARRELL A PAYNE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
