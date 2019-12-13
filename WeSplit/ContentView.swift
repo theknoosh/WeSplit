fc30adbdc6c0d6b91479d71a16e1c993af1329de
Darrell Payne <darrellpayne@earthlink.net>
2019-12-13 08:03:23 -0800

    Day 1 and part of Day 2 completed

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,6 +9,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
         Text("Hello, World!")
     }
